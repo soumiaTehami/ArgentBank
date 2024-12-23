56ee9e169c8496ad7327fd12a3fc52f6d3cbc75f
tehami soumia <sou_mia_46@hotmail.fr>
2024-12-23 07:47:55 +0100

    Footer

---
+++ Frontend/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Copyright © 2024 Argent Bank</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 
-import Header from '../components/Header'; // Chemin vers le composant Header
-
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 function App() {
   return (
     <>
       <Header />
+      <Footer />
       
     </>
   );
